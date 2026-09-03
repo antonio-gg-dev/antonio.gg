d4503e7398c744ea478aafc86b506715dd937de3
antonio-gg-dev <dev@antonio.gg>
2026-09-03 19:26:15 +0200

    chore(deps): update project dependencies and tooling

---
+++ eslint.config.js
@@ -0,0 +1,92 @@
+import js from '@eslint/js'
+import love from 'eslint-config-love'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
+import storybook from 'eslint-plugin-storybook'
+import vue from 'eslint-plugin-vue'
+import globals from 'globals'
+import typescriptEslint from 'typescript-eslint'
+import vueParser from 'vue-eslint-parser'
+
+const sourceFiles = ['**/*.ts']
+const vueFiles = ['**/*.vue']
+const browserGlobals = {
+  ...globals.browser,
+  ...globals.es2021,
+}
+const legacyLoveCompatibilityRules = {
+  '@typescript-eslint/class-methods-use-this': 'off',
+  '@typescript-eslint/init-declarations': 'off',
+  '@typescript-eslint/max-params': 'off',
+  '@typescript-eslint/no-empty-function': 'off',
+  '@typescript-eslint/no-import-type-side-effects': 'off',
+  '@typescript-eslint/no-magic-numbers': 'off',
+  '@typescript-eslint/no-redundant-type-constituents': 'off',
+  '@typescript-eslint/no-unnecessary-condition': 'off',
+  '@typescript-eslint/no-unsafe-assignment': 'off',
+  '@typescript-eslint/no-unsafe-call': 'off',
+  '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+  '@typescript-eslint/no-unsafe-member-access': 'off',
+  '@typescript-eslint/no-unsafe-return': 'off',
+  '@typescript-eslint/no-unsafe-type-assertion': 'off',
+  '@typescript-eslint/prefer-destructuring': 'off',
+  '@typescript-eslint/unbound-method': 'off',
+  complexity: 'off',
+  'max-lines': 'off',
+  'no-console': 'off',
+  'no-negated-condition': 'off',
+  'no-param-reassign': 'off',
+  'no-plusplus': 'off',
+  'prefer-exponentiation-operator': 'off',
+  'prefer-named-capture-group': 'off',
+  'require-unicode-regexp': 'off',
+}
+
+export default [
+  {
+    ignores: [
+      '.agents/snapshots/**',
+      '.vitepress/cache/**',
+      '.vitepress/dist/**',
+      'storybook-static/**',
+    ],
+  },
+  js.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    ...love,
+    files: sourceFiles,
+    languageOptions: {
+      ...love.languageOptions,
+      globals: browserGlobals,
+    },
+    rules: {
+      ...love.rules,
+      ...legacyLoveCompatibilityRules,
+    },
+  },
+  {
+    files: vueFiles,
+    languageOptions: {
+      globals: browserGlobals,
+      parser: vueParser,
+      parserOptions: {
+        extraFileExtensions: ['.vue'],
+        parser: typescriptEslint.parser,
+        projectService: true,
+        sourceType: 'module',
+      },
+    },
+    plugins: love.plugins,
+    rules: {
+      ...love.rules,
+      ...legacyLoveCompatibilityRules,
+    },
+  },
+  ...storybook.configs['flat/recommended'],
+  prettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]
